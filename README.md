4c32462e424d2fbd7484471732e8aed9fa1f7145
Martijn Schuijers <martijnschuijers@gmail.com>
2025-11-20 19:33:27 +0100

    chore: setup repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Schuijers' Webstack
+
+A modular web tooling and component ecosystem for modern front-end projects.
---
+++ eslint.config.ts
@@ -0,0 +1,7 @@
+import { schuijers } from '@schuijers/eslint-config'
+
+export default schuijers({
+  typescript: {
+    tsConfigPath: './tsconfig.json',
+  },
+})
