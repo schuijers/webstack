dd138b2fb967d203d3b45d7c6d561042aff7f96d
Martijn Schuijers <martijnschuijers@gmail.com>
2026-02-06 14:34:41 +0100

    feat(storybook): add a11y addon

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
   addons: [
+    '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-themes',
